029a543dfe56e4693603c5d3865869d52e0d3591
Bijay Nayak <bijay6779@gmail.com>
2023-03-16 23:02:21 +0530

    Downloader Functionality

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask
+
+from routes.downloader import download
 from routes.thumbnails import thumbnail
 
 
 def create_app():
     app = Flask(__name__)
     app.register_blueprint(thumbnail)
+    app.register_blueprint(download)
+
     return app
