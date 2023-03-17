d75b0792ff9971dc8bb51ba60fb974bbe4c0e576
Bijay Nayak <bijay6779@gmail.com>
2023-03-17 05:39:52 +0000

    CORS Fix

--- routes/thumbnails.py
+++ routes/thumbnails.py
@@ -1,8 +1,13 @@
-from flask import Blueprint, render_template
-
+from flask import Blueprint, render_template, request, Response
+import requests
 thumbnail = Blueprint("thumbnail", __name__, url_prefix="/")
 
 
 @thumbnail.route("/")
 def landing():
+    if request.method == "POST":
+        if url := request.form.get("url"):
+            response = requests.get(url)
+            return Response(response.content, mimetype='image/jpeg', headers={'Content-Disposition': 'attachment;filename=image.jpg'})
+
     return render_template("index.html")
