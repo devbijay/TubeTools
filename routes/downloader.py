c01eb2897496bf671b45581dcbe3aef4ebd60d14
Bijay Nayak <bijay6779@gmail.com>
2023-06-20 17:43:12 +0530

    Caption part inprogress

--- routes/downloader.py
+++ routes/downloader.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, request, jsonify
-from pytube import YouTube
+from pytube import YouTube, Caption
 import re
 
 download = Blueprint("download", __name__, url_prefix="/download")
@@ -47,8 +47,34 @@ def video():
                               "size": f"{stream.filesize / (1024 * 1024):.2f} MB",
                               "url": stream.url}
                 no_audio.append(video_data)
+
             video_dict = {"av": with_audio, "ov": no_audio}
-            print(video_dict)
             return jsonify(video_dict)
 
     return render_template("download_video.html")
+
+@download.route("/captions", methods=["GET", "POST"])
+def trim_audio():
+    if request.method == "POST":
+        payload = request.get_json()
+        if video_id := payload.get("video_id"):
+            yt = YouTube(url=f"https://youtube.com/watch?v={video_id}")
+            yt.bypass_age_gate()
+            video_streams_list = stream = yt.streams.get_highest_resolution()
+
+            # Step 5: Generate the trimmed stream URL
+            trimmed_stream_url = f"{stream.url}&begin={10}&end={20}"
+            print("Trimmed Stream URL:", trimmed_stream_url)
+
+    if video_id := request.args.get("video_id"):
+        yt = YouTube(url=f"https://youtube.com/watch?v={video_id}")
+        yt.bypass_age_gate()
+        if captions := yt.captions.all():
+            # Step 5: Select the first available caption track (assuming it's in English)
+            caption = captions[0]  # 'a.en' represents English captions, you can modify it as needed
+
+            # Step 6: Download the captions
+            caption_text = caption.generate_srt_captions()
+            print(caption_text)
+            return caption_text
+
